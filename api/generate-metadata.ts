8ac5c5f432286245a81fb2013128a2fe858489c1
Kunal5s <kunalsonpitre555@gmail.com>
2025-09-24 19:38:52 +0530

    Delete api/generate-metadata.ts

--- api/generate-metadata.ts
+++
@@ -1,7 +0,0 @@
-// This API endpoint is obsolete and no longer in use.
-import type { VercelRequest, VercelResponse } from '@vercel/node';
-
-export default function handler(req: VercelRequest, res: VercelResponse) {
-    res.setHeader('Allow', []);
-    return res.status(410).json({ message: 'This endpoint is no longer available.' });
-}
