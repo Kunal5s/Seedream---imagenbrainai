c5e1493ffd2dc48c2718bf4d76de0f97bc148c5e
Kunal5s <kunalsonpitre555@gmail.com>
2025-09-27 22:14:25 +0530

    feat: Remove user marketplace link from footer
    Removes the "User Marketplace" link from the footer navigation. This is to consolidate marketplace links under a single entry point ("Pollinations Market").

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,4 +1,5 @@
 
+
 import React from 'react';
 import { Link } from 'react-router-dom';
 import FooterBlogFeed from './FooterBlogFeed';
@@ -11,7 +12,6 @@ const Footer: React.FC = () => {
     { name: 'Help Center', path: '/help' },
     { name: 'Community', path: '/community' },
     { name: 'Blog', path: '/blog' },
-    { name: 'User Marketplace', path: '/marketplace' },
     { name: 'Pollinations Market', path: '/pollinations-marketplace' },
   ];
 
